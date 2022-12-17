7f7490a04581cc87bfa5a79fc742376fd7ae0067
Shakya7 <shakyasarkar72@gmail.com>
2022-12-18 00:10:24 +0530

    Implemented API for todo title and priority update

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -18,22 +18,7 @@ exports.createSingleTodo=async (req,res)=>{
         })
     }
 }
-exports.getTodo=async(req,res)=>{
-    try{
-        const todo=await Todo.findById(req.params.id).populate("tasks");
-        res.status(200).json({
-            status:"success",
-            data:{
-                todo
-            }
-        })
-    }catch(err){
-        res.status(400).json({
-            status:"failed",
-            message: err.message
-        })
-    }
-}
+
 
 exports.getAllTodos=async (req,res)=>{
     try{
@@ -55,57 +40,13 @@ exports.getAllTodos=async (req,res)=>{
     
 }
 
-exports.updateSingleTodo=async (req,res)=>{
+exports.updateTodo=async(req,res)=>{
     try{
-        const {id}=req.params;
-        const updatedTodo=await Todo.findByIdAndUpdate(id,{
-            title:req.body.title,
-            color:req.body.color
-        },{
-            new:true,
-            runValidators:true
-        })
+        const todo=await Todo.findByIdAndUpdate(req.params.id,req.body,{new:true});
         res.status(200).json({
-            status:"success",
-            data:{
-                todo:updatedTodo
-            }
+            todo
         })
-    }catch(err){
-        res.status(400).json({
-            status:"failed",
-            message: err.message
-        })
-    }
-}
 
-exports.deleteSingleTodo=async(req,res)=>{
-    try{
-        const {id}=req.params;
-        await Todo.findByIdAndDelete(id);
-        res.status(204).json({
-            status:"success",
-            data:{
-                message:"Todo deleted successfully"
-            }
-        })
-    }catch(err){
-        res.status(400).json({
-            status:"failed",
-            message: err.message
-        })
-    }
-}
-
-exports.deleteAllTodos=async(req,res)=>{
-    try{
-        await Todo.deleteMany();
-        res.status(204).json({
-            status:"success",
-            data:{
-                message:"Todos deleted successfully"
-            }
-        })
     }catch(err){
         res.status(400).json({
             status:"failed",
--- routers/todoRouter.js
+++ routers/todoRouter.js
@@ -3,12 +3,10 @@ const todoController=require("../controllers/todoController");
 
 const router=express.Router();
 
+//Todo routes
 router.route("/createTodo").post(todoController.createSingleTodo);
 router.route("/getAllTodos/:id").get(todoController.getAllTodos);
-router.route("/getTodo/:id").get(todoController.getTodo);
-router.route("/updateSingleTodo/:id").patch(todoController.updateSingleTodo);
-router.route("/deleteSingleTodo/:id").delete(todoController.deleteSingleTodo);
-router.route("/deleteAllTodos").delete(todoController.deleteAllTodos);
+router.route("/updateTodo/:id").patch(todoController.updateTodo);
 
 
 
